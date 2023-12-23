43c6d997ee84881f241adabb9667ba4cdf673f04
Cindy's Desktop <cindy.um73@gmail.com>
2023-12-23 19:15:46 +1100

    started on reducers

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -12,8 +12,9 @@ function UserResults() {
     if (!loading) {
         return (
             <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 
-            lg:grid-cols-3 md: grid-cols-2'>{users.map((user) => (
-                <UserItem key={user.id} user={user}/>
+            lg:grid-cols-3 md: grid-cols-2'>
+                {users.map((user) => (
+                    <UserItem key={user.id} user={user}/>
             ))}
             </div>
           )
