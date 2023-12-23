cb314d47da09e10b3a011777e0395d728fb379ed
Cindy's Desktop <cindy.um73@gmail.com>
2023-12-23 14:04:27 +1100

    added context

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
-
+import { GithubProvider } from './context/github/GithubContext';
 
 function App() {
   return (
-    <>
+    <GithubProvider>
       <Router>
         <div className='flex flex-col justify-between h-screen'>
           <Navbar/>
@@ -26,7 +26,7 @@ function App() {
           <Footer/>
         </div>
       </Router>
-    </>
+    </GithubProvider>
   );
 }
 
