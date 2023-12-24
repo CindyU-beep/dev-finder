fee1213f7b00e5dff4831679046e3aa498da4a07
Cindy's Desktop <cindy.um73@gmail.com>
2023-12-24 14:01:29 +1100

    implement reducers

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,14 +1,10 @@
-import {useEffect, useContext} from 'react'
+import {useContext} from 'react'
 import Loader from '../layout/Loader'
 import UserItem from './UserItem'
 import GithubContext from '../../context/github/GithubContext'
 
 function UserResults() {
-    const {users, loading, fetchUsers} = useContext(GithubContext)
-    useEffect(() => {
-        fetchUsers()
-    }, [fetchUsers])
-
+    const {users, loading} = useContext(GithubContext)
     if (!loading) {
         return (
             <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import GithubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
 
@@ -6,10 +7,16 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 export const GithubProvider = ({children}) => {
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
-    
+    const initialState = {
+        users: [],
+        loading: false
+    }
+    const [state, dispatch] = useReducer(GithubReducer, initialState) 
+    const setLoading = () => dispatch({type: 'SET_LOADING'})
+
+    //get users for testing
     const fetchUsers = async () => {
+        setLoading()
         const reponse = await fetch(`${GITHUB_URL}/users`, {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
@@ -17,12 +24,16 @@ export const GithubProvider = ({children}) => {
         })
 
         const data = await reponse.json()
-        setUsers(data)
-        setLoading(false)
+        //dispatch is a function that we can use to dispatch objects to the reducer
+        dispatch({
+            type: 'GET_USERS',
+            payload: data //payload is the data that we want to send to the reducer
+        })
     }
+
     return <GithubContext.Provider value={{
-        users, 
-        loading,
+        users: state.users,
+        loading: state.loading,
         fetchUsers
     }}>
         {children}
